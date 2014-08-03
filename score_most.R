48ef76e563e60284cb14edacf05948fc60b02874
James Curley <jc3181@columbia.edu>
2014-08-02 22:52:40 -0400

    yet more functions

---
+++ score_most.R
@@ -0,0 +1,36 @@
+### This returns the team who has been involved in the most games of each scoreline
+
+
+score_most<-function (df, score){
+  
+  library(dplyr)
+  library(tidyr)
+
+  temp<-strsplit(score,split="-")
+  temp<-as.vector(unlist(temp[[1]]))
+  score1<-paste(temp[2],temp[1],sep="-")
+  
+  df %>%
+    filter(FT == score | FT == score1) %>%
+    select(Season, home, visitor) %>%
+    gather(Season, team, home:visitor) %>%
+    group_by(team)  %>%
+    tally %>%
+    arrange(desc(n)) %>%
+    filter (n==max(n))  
+}
+
+#Examples
+
+score_most(df, "6-6") # Arsenal 1  Charlton Athletic 1  Leicester City 1  Middlesbrough 1 
+score_most(df, "5-5") # Blackburn Rovers 3   West Ham United 3
+score_most(df, "4-4") # Tottenham Hotspur 14
+score_most(df, "3-3") # Manchester City 68   Wolverhampton Wanderers 68
+score_most(df, "2-2") # Leicester City 274
+score_most(df, "1-1") # Sheffield United 560
+score_most(df, "0-0") # Notts County 363
+
+score_most(df, "1-0") # Birmingham City 795 - most involved in 1-0 or 0-1 games
+score_most(df, "8-0") # Arsenal 7 - most involved in 8-0 or 0-8 games
+score_most(df, "9-1") # Notts County 4 - most involved in 4-1 or 1-4 games
+
---
+++ score_team.R
@@ -0,0 +1,27 @@
+#### List all occurrences of a specific scoreline for a specific team ####
+
+score_team<-function (df, score, teamname) {
+  library(dplyr)
+  library(tidyr)
+  
+  temp<-strsplit(score,split="-")
+  temp<-as.vector(unlist(temp[[1]]))
+  score1<-paste(temp[2],temp[1],sep="-")
+  
+  df %>%
+    filter(FT==score & home==teamname | FT==score1 & visitor==teamname) %>%
+    select(Date, Season, home, visitor, FT, division, tier) %>%
+    arrange(Season)
+  
+}
+
+
+#Examples
+score_team (df,"4-4", "Tottenham Hotspur") #all 4-4 draws Tottenham Hotspur have played in (home and away)
+score_team (df,"3-5", "York City")  #list all 5-3 defeats suffered by York City (regardless of if occurred home/away)
+score_team (df,"5-3", "York City")  #list all 5-3 victories by York City (regardless of if occurred home/away)
+score_team (df,"8-0", "Arsenal") #list all 8-0 victories by Arsenal (regardless of if occurred home/away)
+score_team (df,"0-8", "Arsenal") #list all 8-0 defeats suffered by Arsenal (regardless of if occurred home/away)
+
+
+
