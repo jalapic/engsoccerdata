8905b90fb2adf54cc189e4da0665748bc61ff699
James Curley <jc3181@columbia.edu>
2014-09-03 17:31:42 -0400

    new functions

---
+++ R.functions/bestwins.R
@@ -0,0 +1,42 @@
+# Return each team's record win (top 5 etc)
+
+
+bestwins<-function(df, teamname, type=NULL, N=NULL){
+  
+N<- if(is.null(N))  10 else {N}
+
+library(tidyr)
+library(dplyr)
+  
+if(is.null(type)) 
+
+df %>%
+  filter(home==teamname & result=="H" | visitor==teamname & result=="A") %>%
+  mutate(maxgoal=pmax(hgoal, vgoal), mingoal=pmin(hgoal,vgoal), absgoaldif=abs(goaldif)) %>%
+  arrange(desc(absgoaldif),desc(maxgoal)) %>%
+  select (Season, home, visitor, FT, division) %>%
+  head(N)
+
+else
+  
+{
+
+  df %>%
+  filter(home==teamname & result=="H" | visitor==teamname & result=="A") %>%
+  mutate(maxgoal=pmax(hgoal, vgoal), mingoal=pmin(hgoal,vgoal), absgoaldif=abs(goaldif)) %>%
+  arrange(desc(absgoaldif),desc(maxgoal)) %>%
+  select (Season, home, visitor, FT, division) %>%
+  filter (result==type) %>%
+  head(N)
+
+  
+}
+}
+
+  
+#
+bestwins(df,"Everton")
+bestwins(df,"Aston Villa", type="H")
+bestwins(df,"York City", type="A")
+bestwins(df,"Port Vale", N=20)
+bestwins(df,"Hull City", type="A", N=7)
---
+++ R.functions/worstlosses.R
@@ -0,0 +1,43 @@
+# Return each team's worst losses
+
+
+worstlosses<-function(df, teamname, type=NULL, N=NULL){
+
+  library(dplyr)
+  library(tidyr)
+  
+  N<- if(is.null(N))  10 else {N}
+  
+  
+  if(is.null(type)) 
+    
+    df %>%
+    filter(home==teamname & result=="A" | visitor==teamname & result=="H") %>%
+    mutate(maxgoal=pmax(hgoal, vgoal), mingoal=pmin(hgoal,vgoal), absgoaldif=abs(goaldif)) %>%
+    arrange(desc(absgoaldif),desc(maxgoal)) %>%
+    select (Season, home, visitor, FT, division) %>%
+    head(N)
+  
+  else
+    
+  {
+    
+    df %>%
+      filter(home==teamname & result=="A" | visitor==teamname & result=="H") %>%
+      mutate(maxgoal=pmax(hgoal, vgoal), mingoal=pmin(hgoal,vgoal), absgoaldif=abs(goaldif)) %>%
+      arrange(desc(absgoaldif),desc(maxgoal)) %>%
+      select (Season, home, visitor, FT, division) %>%
+      filter (result==type) %>%
+      head(N)
+    
+    
+  }
+}
+
+
+
+worstlosses(df,"Everton")
+worstlosses(df,"Aston Villa", type="H")
+worstlosses(df,"York City", type="A")
+worstlosses(df,"Port Vale", N=20)
+worstlosses(df,"Hull City", type="A", N=7)
