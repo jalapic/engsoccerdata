8f3b8894bc43dc9be79fccd6e7cb0cf5e5ba71e1
Håkon Malmedal <hmalmedal@gmail.com>
2014-10-06 10:28:41 +0200

    games_by_tier

--- R/games_by_tier.R
+++ R/games_by_tier.R
@@ -1,37 +1,36 @@
-#### Number of Games in Each Tier
-
+#' Number of Games in Each Tier
+#'
+#' @examples
+#' games_by_tier(df, 1)
+#' games_by_tier(df, 2)
+#' games_by_tier(df, 3)
+#' games_by_tier(df, 4)
+#'
+#' as.data.frame(unclass(games_by_tier(df))) #all games by all teams in all tiers
+#'
+#' @export
 games_by_tier<-function(df,Tier=NULL){
-  
-  if(is.null(Tier)) 
+
+  if(is.null(Tier))
     df %>%
     select(Season, tier, home, visitor) %>%
     gather (venue, team, home:visitor) %>%
     group_by(team,tier) %>%
     summarise(total=n()) %>%
     arrange (desc(total))
-  
+
   else
-    
+
   {
     dfTier<-df %>%
-     filter(tier==Tier) 
-   
+     filter(tier==Tier)
+
    dfTier %>%
      select(Season, tier, home, visitor) %>%
      gather (venue, team, home:visitor) %>%
      group_by(team,tier) %>%
      summarise(total=n()) %>%
      arrange (desc(total))
-   
+
   }
 }
-
-
-# Examples
-
-games_by_tier(df, 1)
-games_by_tier(df, 2)
-games_by_tier(df, 3)
-games_by_tier(df, 4)
-
-as.data.frame(unclass(games_by_tier(df))) #all games by all teams in all tiers
