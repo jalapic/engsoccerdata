cd27592763ee63849ea20092c558c7d89a51415a
Håkon Malmedal <hmalmedal@gmail.com>
2014-10-06 11:43:55 +0200

    Document datasets

--- R/engsoccerdata-package.R
+++ R/engsoccerdata-package.R
@@ -4,3 +4,9 @@
 #' @docType package
 #' @import dplyr tidyr
 NULL
+
+#' Results of all top 4 tier soccer games in England 1888-2014
+"engsoccerdata2"
+
+#' List of 142 teams plus whether they were in the top 4 divisions in 2013/4
+"engsoccerteams"
