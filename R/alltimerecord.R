30d9493691c0bb6272332d01700a33c8dbe1ef97
Håkon Malmedal <hmalmedal@gmail.com>
2014-10-06 10:20:26 +0200

    alltimerecord

--- R/alltimerecord.R
+++ R/alltimerecord.R
@@ -1,31 +1,32 @@
-### Function to List the all-time records of a team
-
+#' Function to List the all-time records of a team
+#'
+#' @examples
+#' alltimerecord(df, "Aston Villa")
+#' alltimerecord(df, "Arsenal")
+#' alltimerecord(df, "Liverpool")
+#' alltimerecord(df, "Manchester United")
+#' alltimerecord(df, "York City")
+#' alltimerecord(df, "Rochdale")
+#' alltimerecord(df, "Birmingham City")
+#' alltimerecord(df, "Leeds City")
+#'
+#' @export
 alltimerecord<-function (df, teamname) {
-  
+
   library(dplyr)
   library(tidyr)
-  
+
   hrec<-df %>%
     filter(home==teamname)  %>%
     summarise(P = n(), W=sum(result=="H"), D=sum(result=="D"), L=sum(result=="A"),
               GF = sum(hgoal), GA = sum(vgoal), GD=sum(goaldif))
-  
+
   vrec<-df %>%
     filter(visitor==teamname)  %>%
     summarise(P = n(),  W=sum(result=="A"), D=sum(result=="D"), L=sum(result=="H"),
               GF = sum(vgoal), GA = sum(hgoal), GD=GF-GA,)
-  
-  temp<-rbind(hrec,vrec,hrec+vrec)  
+
+  temp<-rbind(hrec,vrec,hrec+vrec)
   rownames(temp)<-c("home", "away", "total")
   return(temp)
 }
-
-#Examples
-alltimerecord(df, "Aston Villa")
-alltimerecord(df, "Arsenal")
-alltimerecord(df, "Liverpool")
-alltimerecord(df, "Manchester United")
-alltimerecord(df, "York City")
-alltimerecord(df, "Rochdale")
-alltimerecord(df, "Birmingham City")
-alltimerecord(df, "Leeds City")
