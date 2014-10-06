319a9514a32b1f48750d7496eee6eb6248d956cd
Håkon Malmedal <hmalmedal@gmail.com>
2014-10-06 10:33:03 +0200

    namecheck

--- R/namecheck.R
+++ R/namecheck.R
@@ -1,25 +1,26 @@
-### Function to check what characters exist in which team names
-
+#' Function to check what characters exist in which team names
+#'
+#' @examples
+#' namecheck(df, "hester")
+#' namecheck(df, "United")
+#' namecheck(df, "Athletic")
+#' namecheck(df, "Rovers")
+#' namecheck(df, "x")
+#'
+#' @export
 namecheck<-function(df,x){
   teams<-unique(df$home)
   teams.nos<-grep(x, teams)
-  teams.res<- vector("list",length(teams.nos)) 
-  for (i in 1:length(teams.nos)){  
-    
+  teams.res<- vector("list",length(teams.nos))
+  for (i in 1:length(teams.nos)){
+
     y<-teams.nos[[i]]
     teams.res[[i]] <- as.character(teams[[y]])
-  }  
-  
+  }
+
   temp<-as.data.frame(unlist(teams.res))
   colnames(temp)<-c("team")
   library(dplyr)
   temp1 <- temp %>% arrange (team)
-  return(temp1) 
+  return(temp1)
 }
-
-#Examples
-namecheck(df, "hester")
-namecheck(df, "United")
-namecheck(df, "Athletic")
-namecheck(df, "Rovers")
-namecheck(df, "x"
