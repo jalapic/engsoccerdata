fa10169e86cd930ee076527d488aee74a6991957
Håkon Malmedal <hmalmedal@gmail.com>
2014-10-06 10:36:26 +0200

    n_offs

--- R/n_offs.R
+++ R/n_offs.R
@@ -1,10 +1,25 @@
-#### Return Scorelines that only occurred n times in history
-
+#' Return Scorelines that only occurred n times in history
+#'
+#' note don't try this with 0 - won't return infinite possible scorelines
+#' If you pick crazy numbers for the number of times e.g. 34223, you'll get an error message
+#' this function is meant for small numbers of occurrences
+#'
+#' @examples
+#' n_offs(df, 1) #return results that have occurred only once across all four tiers
+#' n_offs(df, 2) #return results that have occurred only twice across all four tiers
+#' n_offs(df, 3) #return results that have occurred 3 times across all four tiers
+#'
+#' n_offs(df, 1, 1) #return which results have occurred only once in the top tier
+#' n_offs(df, 1, 4) #return which results have occurred only once in the 4th tier
+#' n_offs(df, 2, 2) #return which results have occurred twice in the 2nd tier
+#' n_offs(df, 5, 3) #return which results have occurred five times in the 3rd tier
+#'
+#' @export
 n_offs<- function(df, N, Tier=NULL){
   library(dplyr)
   library(tidyr)
-  
-  if(is.null(Tier)) 
+
+  if(is.null(Tier))
     df %>%
     group_by(FT)%>%
     tally %>%
@@ -13,12 +28,12 @@ n_offs<- function(df, N, Tier=NULL){
     left_join(df) %>%
     select (Date, Season, home, visitor, FT, division, tier, result)%>%
     arrange(FT, Season)
-  
+
   else
-  
+
     {dfTier<-df %>%
-    filter(tier==Tier) 
-  
+    filter(tier==Tier)
+
     dfTier%>%
     group_by(FT)%>%
     tally %>%
@@ -29,17 +44,3 @@ n_offs<- function(df, N, Tier=NULL){
     arrange(FT, Season)
     }
 }
-
-n_offs(df, 1) #return results that have occurred only once across all four tiers
-n_offs(df, 2) #return results that have occurred only twice across all four tiers
-n_offs(df, 3) #return results that have occurred 3 times across all four tiers
-
-n_offs(df, 1, 1) #return which results have occurred only once in the top tier
-n_offs(df, 1, 4) #return which results have occurred only once in the 4th tier
-n_offs(df, 2, 2) #return which results have occurred twice in the 2nd tier
-n_offs(df, 5, 3) #return which results have occurred five times in the 3rd tier
-
-
-#note don't try this with 0 - won't return infinite possible scorelines
-#If you pick crazy numbers for the number of times e.g. 34223, you'll get an error message
-#this function is meant for small numbers of occurrences
