a892c7d97fb42f8663b975382b663ce0ddb4c4ef
Håkon Malmedal <hmalmedal@gmail.com>
2014-10-06 10:41:06 +0200

    opponents

--- R/opponents.R
+++ R/opponents.R
@@ -1,12 +1,17 @@
-# Total number of different opponents
-
-library(dplyr)
-library(tidyr)
-
+#' Total number of different opponents
+#'
+#' @examples
+#' opponents(df)  #Grimsby Town and Lincoln City have the most unique opponents - 134 each
+#' opponents(df,1)
+#' opponents(df,2)
+#' opponents(df,3)
+#' opponents(df,4)
+#'
+#' @export
 opponents<-function(df,Tier=NULL){
-  
-  if(is.null(Tier)) 
-    
+
+  if(is.null(Tier))
+
 rbind (df %>%select(team1=home,team2=visitor), df %>%select(team1=visitor,team2=home)) %>%
   group_by(team1) %>%
   summarise(Opponents=n_distinct(team2)) %>%
@@ -14,7 +19,7 @@ rbind (df %>%select(team1=home,team2=visitor), df %>%select(team1=visitor,team2=
 
 
 else
-  
+
 { rbind (df %>%select(team1=home,team2=visitor,tier), df %>%select(team1=visitor,team2=home,tier)) %>%
    filter(tier==Tier) %>%
    group_by(team1) %>%
@@ -22,10 +27,3 @@ else
    arrange(desc(Opponents))
 }
 }
-  
-#Examples
-opponents(df)  #Grimsby Town and Lincoln City have the most unique opponents - 134 each
-opponents(df,1)
-opponents(df,2)
-opponents(df,3)
-opponents(df,4)
