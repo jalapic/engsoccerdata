6acfcb1cb51db05e1e6137bbc59c069a7ba40708
James Curley <jc3181@columbia.edu>
2015-05-26 21:29:52 -0400

    update 2014-15 English League Data
    update 2014-15 English League Data

--- R/engsoccerdata-package.R
+++ R/engsoccerdata-package.R
@@ -5,7 +5,7 @@
 #' @import dplyr tidyr
 NULL
 
-#' Results and dates of all top 4 tier soccer games in England 1888-2014
+#' Results and dates of all top 4 tier soccer games in England 1888-2015
 "engsoccerdata2"
 
 #' List of 142 teams plus whether they were in the top 4 divisions in 2013/4
