9012bf9f0f2fd59c1d0b977f90be7b9d92c5cfd4
James Curley <jc3181@columbia.edu>
2017-01-13 22:52:17 -0500

    add current season function and France data

---
+++ R/england_current.R
@@ -0,0 +1,41 @@
+#' Get current England season data for all tiers
+#'
+#' @return a dataframe with results for current
+#' season for all top four divisions
+#' @importFrom utils "read.csv"
+#' @examples
+#' england_current()
+#' @export
+
+
+england_current <- function(){
+
+  teamnames<-tm<-df1<-df<-.<-Date<-tier<-home<-visitor<-hgoal<-vgoal<-goaldif<-FT<-Season<-division<-result<-name<-name_other<-most_recent<-country<-NULL
+
+  df <- rbind(read.csv("http://www.football-data.co.uk/mmz4281/1617/E0.csv"),
+              read.csv("http://www.football-data.co.uk/mmz4281/1617/E1.csv"),
+              read.csv("http://www.football-data.co.uk/mmz4281/1617/E2.csv"),
+              read.csv("http://www.football-data.co.uk/mmz4281/1617/E3.csv")
+  )
+
+  df1 <-  data.frame("Date" = as.character(as.Date(df$Date, "%d/%m/%y")),
+                     "Season" = 2016,
+                     "home" = as.character(df$HomeTeam),
+                     "visitor" = as.character(df$AwayTeam),
+                     "FT" = paste0(df$FTHG, "-", df$FTAG),
+                     "hgoal" = df$FTHG,
+                     "vgoal" = df$FTAG,
+                     "division" = as.numeric(df$Div),
+                     "tier" = as.numeric(df$Div),
+                     "totgoal" = df$FTHG + df$FTAG,
+                     "goaldif" = df$FTHG - df$FTAG,
+                     "result" = as.character(ifelse(df$FTHG > df$FTAG, "H",
+                                                    ifelse(df$FTHG < df$FTAG, "A", "D")))
+                     , stringsAsFactors = FALSE)
+
+  tm <- teamnames[teamnames$name!="Accrington F.C.",]
+  df1$home <- tm$name[match(df1$home,tm$name_other)]
+  df1$visitor <- tm$name[match(df1$visitor,tm$name_other)]
+  return(df1)
+
+}
---
+++ R/teamnames.R
@@ -0,0 +1,13 @@
+#' A dataframe of teamnames and their variants
+#'
+#' Contains variant spellings of teamnames found
+#' in league dataframes
+#'
+#' @format A data frame with 848 rows and 4 variables:
+#' \describe{
+#'   \item{country}{Country of team}
+#'   \item{name}{Name of team used in engsoccerdata}
+#'   \item{name_other}{Name variants found in data sources}
+#'   \item{most_recent}{Whether the name is the most recently used one}
+#' }
+"teamnames"
