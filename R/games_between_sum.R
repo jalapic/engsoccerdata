bf173ede9b328bd1805b59a83e224db118e11c80
James Curley <jc3181@columbia.edu>
2017-05-15 10:10:27 -0400

    games_between_sum update

--- R/games_between_sum.R
+++ R/games_between_sum.R
@@ -3,28 +3,47 @@
 #' @param df The results dataframe
 #' @param teamname1 teamname1
 #' @param teamname2 teamname2
+#' @param type Return 'all' results when type=\code{all} or split by
+#'     home and away if type=\code{homeaway}.
 #' @return a dataframe with summary of results. Will be
-#' empty dataframe if never played.
+#'     empty dataframe if never played.
 #' @importFrom magrittr "%>%"
 #' @examples
-#' games_between_sum(england, "Exeter City", "York City")
-#' games_between_sum(england, "Aston Villa", "York City")
-#' games_between_sum(england, "Manchester United", "Liverpool")
+#'     games_between_sum(england, "Exeter City", "York City")
+#'     games_between_sum(england, "Aston Villa", "York City", "homeaway")
+#'     games_between_sum(england, "Manchester United", "Liverpool", "all")
 #' @export
 
 
-games_between_sum<-function (df=NULL, teamname1=NULL, teamname2=NULL) {
+games_between_sum<-function (df=NULL, teamname1=NULL, teamname2=NULL, type=c("all", "homeaway")) {
 
-  .<-home<-visitor<-hgoal<-vgoal<-goaldif<-FT<-Season<-division<-result<-maxgoal<-mingoal<-absgoaldif<-NULL
+  D<-GA<-GD<-GF<-L<-W<-ga<-gf<-opp<-team<-venue<-tmp1<-tmp2<-tmp<-.<-home<-visitor<-hgoal<-vgoal<-goaldif<-FT<-Season<-division<-result<-maxgoal<-mingoal<-absgoaldif<-NULL
 
 
   tmp<- df %>%
     dplyr::group_by(home) %>%
-    dplyr::filter(home==teamname1 & visitor==teamname2 | home==teamname2 & visitor==teamname1) %>%
-    dplyr::mutate(goaldif  = hgoal-vgoal,
-                  result = ifelse(hgoal>vgoal, "H", ifelse(hgoal<vgoal, "A", "D"))) %>%
-    dplyr::summarise(P = nrow(.), GF = sum(hgoal), GA = sum(vgoal), GD=sum(goaldif),
-              W=sum(result=="H"), D=sum(result=="D"), L=sum(result=="A") )
+    dplyr::filter(home==teamname1 & visitor==teamname2 | home==teamname2 & visitor==teamname1)
 
-  return(as.data.frame(tmp))
-  }
+  engsoccerdata::homeaway(tmp) %>%
+    dplyr::group_by(team,opp,venue) %>%
+    dplyr::mutate(goaldif  = gf-ga,
+                  result = ifelse(gf>ga, "H", ifelse(gf<ga, "A", "D"))) %>%
+    dplyr::summarise(P = nrow(.), GF = sum(gf), GA = sum(ga), GD=sum(goaldif),
+                     W=sum(result=="H"), D=sum(result=="D"), L=sum(result=="A") ) %>%
+    dplyr::select(team,opp,venue,W,D,L,GF,GA,GD) %>%
+    as.data.frame() -> tmp1
+
+
+  engsoccerdata::homeaway(tmp) %>%
+    dplyr::mutate(venue="all",
+                  goaldif  = gf-ga,
+                  result = ifelse(gf>ga, "H", ifelse(gf<ga, "A", "D"))) %>%
+    dplyr::group_by(team,opp,venue) %>%
+    dplyr::summarise(P = nrow(.), GF = sum(gf), GA = sum(ga), GD=sum(goaldif),
+                     W=sum(result=="H"), D=sum(result=="D"), L=sum(result=="A") ) %>%
+    dplyr::select(team,opp,venue,W,D,L,GF,GA,GD) %>%
+    as.data.frame() -> tmp2
+
+  if(type=="all"){return(tmp2)}
+  if(type=="homeaway"){return(tmp1)}
+}
