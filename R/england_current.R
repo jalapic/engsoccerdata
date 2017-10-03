2543b5b7f6672919d27cda6cce9f6004554208c4
James Curley <jc3181@columbia.edu>
2017-10-02 20:12:42 -0500

    update england_current

--- R/england_current.R
+++ R/england_current.R
@@ -8,47 +8,45 @@
 #' england_current()
 #' @export
 
-england_current <- function(Season=2017){
+england_current1 <- function(Season = 2017){
 
-     s1<-s2<-myseason<-tm<-df1<-df<-.<-Date<-tier<-home<-visitor<-hgoal<-vgoal<-goaldif<-FT<-division<-result<-name<-name_other<-most_recent<-country<-NULL
-     myseason<-Season
-     s2<-as.numeric(substr(myseason,3,4))
-     s1 <- s2+1
+  s1 <- s2 <- myseason <- tm <- df1 <- df <- . <- Date <- tier <- home <- visitor <- hgoal <- vgoal <- goaldif <- FT <- division <- result <- name <- name_other <- most_recent <- country <- NULL
 
-  df <- rbind(read.csv(paste0("http://www.football-data.co.uk/mmz4281/",s2,s1,"/E0.csv")),
-              read.csv(paste0("http://www.football-data.co.uk/mmz4281/",s2,s1,"/E1.csv")),
-              read.csv(paste0("http://www.football-data.co.uk/mmz4281/",s2,s1,"/E2.csv")),
-              read.csv(paste0("http://www.football-data.co.uk/mmz4281/",s2,s1,"/E3.csv"))
+  myseason <- Season
+  s2 <- as.numeric(substr(myseason, 3, 4))
+  s1 <- s2 + 1
+
+  df <- rbind(read.csv(paste0("http://www.football-data.co.uk/mmz4281/ ", s2, s1, "/E0.csv")),
+              read.csv(paste0("http://www.football-data.co.uk/mmz4281/ ", s2, s1, "/E1.csv")),
+              read.csv(paste0("http://www.football-data.co.uk/mmz4281/ ", s2, s1, "/E2.csv")),
+              read.csv(paste0("http://www.football-data.co.uk/mmz4281/ ", s2, s1, "/E3.csv"))
   )
 
-  engl<- engsoccerdata::england
 
-  if(identical(max(as.Date(df$Date, "%d/%m/%y")), max(engl$Date) )) warning("The returned dataframe contains data already included in 'england' dataframe")
+  engl <- engsoccerdata::england
+  if (identical(max(as.Date(df$Date, "%d/%m/%y")), max(engl$Date)))
+    warning("The returned dataframe contains data already included in 'england' dataframe")
 
+  df1 <- data.frame(Date = as.character(as.Date(df$Date, "%d/%m/%y")),
+                    Season = myseason, home = as.character(df$HomeTeam),
+                    visitor = as.character(df$AwayTeam), FT = paste0(df$FTHG,
+                                                                     "-", df$FTAG), hgoal = df$FTHG, vgoal = df$FTAG,
+                    division = as.numeric(factor(df$Div)), tier = as.numeric(factor(df$Div)),
+                    totgoal = df$FTHG + df$FTAG, goaldif = df$FTHG - df$FTAG,
+                    result = as.character(ifelse(df$FTHG > df$FTAG, "H",
+                                                 ifelse(df$FTHG < df$FTAG, "A", "D"))))
+  return(df1)
+}
 
-  df1 <-  data.frame("Date" = as.character(as.Date(df$Date, "%d/%m/%y")),
-                     "Season" = myseason,
-                     "home" = as.character(df$HomeTeam),
-                     "visitor" = as.character(df$AwayTeam),
-                     "FT" = paste0(df$FTHG, "-", df$FTAG),
-                     "hgoal" = df$FTHG,
-                     "vgoal" = df$FTAG,
-                     "division" = as.numeric(factor(df$Div)),
-                     "tier" = as.numeric(factor(df$Div)),
-                     "totgoal" = df$FTHG + df$FTAG,
-                     "goaldif" = df$FTHG - df$FTAG,
-                     "result" = as.character(ifelse(df$FTHG > df$FTAG, "H",
-                                                    ifelse(df$FTHG < df$FTAG, "A", "D")))
-                     , stringsAsFactors = FALSE)
+england_current2 <- function(df1) {
+  tm <- teamnames[teamnames$name != "Accrington F.C.", ]
+  df1$home <- tm$name[match(df1$home, tm$name_other)]
+  df1$visitor <- tm$name[match(df1$visitor, tm$name_other)]
+  df1$Date <- as.Date(df1$Date, format = "%Y-%m-%d")
+  return(df1)
+}
 
-
-  tm <- teamnames[teamnames$name!="Accrington F.C.",]
-
-  df1$home <- tm$name[match(df1$home,tm$name_other)]
-  df1$visitor <- tm$name[match(df1$visitor,tm$name_other)]
-
-  df1$Date <- as.Date(df1$Date, format="%Y-%m-%d")
-
-    return(df1)
-
+england_current <-  function(Season=2017){
+  d <- england_current2(england_current1(Season=Season))
+  return(d)
 }
