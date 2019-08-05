0d00242e8ab14e17fd1601dbb0e530d54ed68cc1
RobWHickman <robwhickman@gmail.com>
2019-08-05 11:28:40 +0100

    updated all data

---
+++ R/leaguecup.R
@@ -0,0 +1,25 @@
+#' English league playoff results 1892-2017
+#'
+#' All results for English league playoff games in the top tier
+#' from 1892/93 season to 2016/17 season.
+#'
+#' @format A data frame with 16871 rows and 19 variables:
+#' \describe{
+#'   \item{Date}{Date of match}
+#'   \item{Season}{Season of match - refers to starting year}
+#'   \item{home}{Home team}
+#'   \item{visitor}{Visiting team}
+#'   \item{FT}{Full-time result}
+#'   \item{hgoal}{Goals scored by home team}
+#'   \item{vgoal}{Goals scored by visiting team}
+#'   \item{round}{Proper Round}
+#'   \item{tie}{Initial tie or replay}
+#'   \item{leg}{Which leg of a two legged match}
+#'   \item{aet}{Did game go to extra time?}
+#'   \item{pen}{Did game go to penalties?}
+#'   \item{pens}{Result of penalties}
+#'   \item{Venue}{Venue}
+#'   \item{attendance}{Attendance}
+#'   \item{northsouth}{}
+#' }
+"leaguecup"
